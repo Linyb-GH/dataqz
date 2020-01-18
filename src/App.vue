41c2c7a5eed4b51b7b46897f7480df51460d4109
Bingo <961549629@qq.com>
2020-01-18 21:54:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar/>
+    <test/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from './components/mainTabbar/MainTabBar'
+  import Test from './components/iview/test'
+
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar,
+      Test
+    }
+  }
+
+
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+</style>
